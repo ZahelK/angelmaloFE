7682e5759dd1b0ea84847053da339d1ccd02d89d
alvaro concha gutierrez <alvaroconcha14@gmail.com>
2025-10-27 00:47:11 -0300

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import axios from "axios";
+import logo from "./assets/AngelLogo.png";
+import '@fontsource/be-vietnam-pro';
 
 function App() {
   const [formData, setFormData] = useState({
     nombre: "",
     rut: "",
     correo: "",
-    numero: ""
+    numero: "",
   });
 
   const handleChange = (e) => {
@@ -16,7 +18,10 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3000/api/form", formData);
+      const res = await axios.post(
+        "https://angelmalo-be.vercel.app/api/form",
+        formData
+      );
       alert("Datos guardados correctamente ✅");
       console.log(res.data);
       setFormData({ nombre: "", rut: "", correo: "", numero: "" });
@@ -27,36 +32,55 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Formulario de Registro</h1>
-      <form onSubmit={handleSubmit}>
+    <>
+    <div
+      className="flex flex-col items-center w-screen justify-center bg-contain bg-center font-vietnam bg-no-repeat"
+      style={{ backgroundImage: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(${logo})` }}
+    >
+      <label className="font-vietnam p-4 font-bold text-2xl">Registrate</label>
+      <form
+        className="flex flex-col justify-center w-100 p-4"
+        onSubmit={handleSubmit}
+      >
+        <label className="font-vietnam font-bold">Nombre:</label>
         <input
           name="nombre"
           placeholder="Nombre"
           value={formData.nombre}
           onChange={handleChange}
+          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
         />
+        <label className="font-vietnam font-bold">RUT:</label>
         <input
           name="rut"
-          placeholder="rut"
+          placeholder="12345678-9"
           value={formData.rut}
           onChange={handleChange}
+          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
         />
+        <label className="font-vietnam font-bold">Correo:</label>
         <input
           name="correo"
-          placeholder="Correo"
+          placeholder="email@example.com"
           value={formData.correo}
           onChange={handleChange}
+          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
         />
+        <label className="font-vietnam font-bold">Número:</label>
         <input
           name="numero"
-          placeholder="numero"
+          placeholder="+56912345678"
           value={formData.numero}
           onChange={handleChange}
+          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60 "
         />
-        <button type="submit">Guardar</button>
+        <button type="submit" className="bg-red-600 m-5 text-white rounded-4xl p-3 text-2xl">
+          Guardar
+        </button>
       </form>
     </div>
+    </>
+    
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <div>Beneficios</div>
+    <p>Acumula puntos dictando tu rut en caja</p>
+    <p>Cada 10</p>
   </StrictMode>,
 )
