77006925aa046bad201531d2b7c9eb843e8183d7
alvaro concha gutierrez <alvaroconcha14@gmail.com>
2025-10-28 14:35:53 -0300

    estilos final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
-import logo from "./assets/AngelLogo.png";
-import '@fontsource/be-vietnam-pro';
+import "@fontsource/be-vietnam-pro";
+import checkIcon from "./assets/check.svg";
 
 function App() {
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showSuccess2, setShowSuccess2] = useState(false);
+  const [showSuccess3, setShowSuccess3] = useState(false);
+  const [showFailded, setShowFailed] = useState(false);
   const [formData, setFormData] = useState({
     nombre: "",
     rut: "",
@@ -22,65 +26,117 @@ function App() {
         "https://angelmalo-be.vercel.app/api/form",
         formData
       );
-      alert("Datos guardados correctamente ✅");
+
+      setShowSuccess(true);
+
+      // Ocultar después de 1 segundo y limpiar campos
+      setTimeout(() => {
+        setShowSuccess(false);
+        setFormData({ nombre: "", rut: "", correo: "", numero: "" });
+        setShowSuccess2(true);
+
+        setTimeout(() => {
+        setShowSuccess2(false);
+        setShowSuccess3(true);
+
+        setTimeout(() => {
+        setShowSuccess3(false);
+      }, 200);
+      }, 1000);
+      }, 200);
+
+
       console.log(res.data);
-      setFormData({ nombre: "", rut: "", correo: "", numero: "" });
     } catch (err) {
-      alert("Error al guardar ❌");
+      showFailded(true);
       console.error(err);
     }
   };
 
+  const handleFailure = () => {
+    
+      setShowFailed(false);
+  };
+
   return (
     <>
-    <div
-      className="flex flex-col items-center w-screen justify-center bg-contain bg-center font-vietnam bg-no-repeat"
-      style={{ backgroundImage: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(${logo})` }}
-    >
-      <label className="font-vietnam p-4 font-bold text-2xl">Registrate</label>
-      <form
-        className="flex flex-col justify-center w-100 p-4"
-        onSubmit={handleSubmit}
-      >
-        <label className="font-vietnam font-bold">Nombre:</label>
-        <input
-          name="nombre"
-          placeholder="Nombre"
-          value={formData.nombre}
-          onChange={handleChange}
-          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
-        />
-        <label className="font-vietnam font-bold">RUT:</label>
-        <input
-          name="rut"
-          placeholder="12345678-9"
-          value={formData.rut}
-          onChange={handleChange}
-          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
-        />
-        <label className="font-vietnam font-bold">Correo:</label>
-        <input
-          name="correo"
-          placeholder="email@example.com"
-          value={formData.correo}
-          onChange={handleChange}
-          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60"
-        />
-        <label className="font-vietnam font-bold">Número:</label>
-        <input
-          name="numero"
-          placeholder="+56912345678"
-          value={formData.numero}
-          onChange={handleChange}
-          className="bg-gray-200/50 rounded-md p-2 mb-2 border-2 border-gray-400/60 "
-        />
-        <button type="submit" className="bg-red-600 m-5 text-white rounded-4xl p-3 text-2xl">
-          Guardar
-        </button>
-      </form>
-    </div>
+      <div className="relative flex flex-col items-center w-screen justify-center font-vietnam">
+        {showSuccess && (
+          <div className="w-100 h-100 absolute flex items-center justify-center z-50 animate-zoom-in  animate-duration-200 bg-none">
+            <img src={checkIcon} alt="Success" className="w-100 h-100" />
+          </div>
+        )}
+
+        {showSuccess2 && (
+          <div className="w-100 h-100 absolute flex items-center justify-center z-50 animate-tada  animate-duration-1000 bg-none">
+            <img src={checkIcon} alt="Success" className="w-100 h-100" />
+          </div>
+        )}
+
+        {showSuccess3 && (
+          <div className="w-100 h-100 absolute flex items-center justify-center z-50 animate-zoom-out  animate-duration-200 bg-none">
+            <img src={checkIcon} alt="Success" className="w-100 h-100" />
+          </div>
+        )}
+
+        {showFailded && (
+          <div className="absolute flex items-center justify-center">
+            <label>Algo salió  mal!</label>
+
+          </div>
+        )}
+
+        <label className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 via-yellow-500 to-amber-600 mt-10">
+          Alameda
+        </label>
+        <label className="font-vietnam  font-semibold text-lg">Botilleria</label>
+        <form
+          className="flex flex-col justify-center w-100 p-2"
+          onSubmit={handleSubmit}
+        >
+          <label className="font-vietnam p-1">Nombre:</label>
+          <input
+            name="nombre"
+            placeholder="Nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            className="bg-white/10 rounded-md p-2 mb-2 border-2 border-none"
+          />
+          <label className="font-vietnam p-1">RUT:</label>
+          <input
+            name="rut"
+            placeholder="12345678-9"
+            value={formData.rut}
+            onChange={handleChange}
+            className="bg-white/10 rounded-md p-2 mb-2 border-2 border-none"
+          />
+          <label className="font-vietnam p-1">
+            Correo Electrónico:
+          </label>
+          <input
+            name="correo"
+            placeholder="email@ejemplo.com"
+            value={formData.correo}
+            onChange={handleChange}
+            className="bg-white/10 rounded-md p-2 mb-2 border-2 border-none"
+          />
+          <label className="font-vietnam p-1">Número:</label>
+          <input
+            name="numero"
+            placeholder="+56912345678"
+            value={formData.numero}
+            onChange={handleChange}
+            className="bg-white/10 rounded-md p-2 mb-2 border-2 border-none"
+          />
+          <button
+            type="submit"
+            className="px-6 py-2 mt-5 rounded-lg text-blue-950 font-semibold bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 shadow-md hover:scale-105 transition"
+          >
+            Guardar
+          </button>
+        </form>
+      </div>
     </>
-    
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <div>Beneficios</div>
-    <p>Acumula puntos dictando tu rut en caja</p>
-    <p>Cada 10</p>
-  </StrictMode>,
-)
+    <div className="flex flex-col justify-center p-4 font-normal text-blue-100">
+      <div className="bg-white/20 p-1 rounded-lg m-4 bg-gradient-to-br from-yellow-400 to-amber-600">
+        <div className="p-2 bg-blue-950 rounded-sm">
+          <label className="font-extrabold text-2xl text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 via-yellow-500 to-amber-600">
+            PROMOCION
+          </label>
+          <p className="text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 via-yellow-500 to-amber-600">
+            Registrate antes del 1 de novienbre para participar por una gif card
+            de 50 luquitas
+          </p>
+        </div>
+      </div>
+      <div className="self-center m-5 text-2xl font-bold">Beneficios</div>
+      <p className="bg-white/10 p-2 rounded-lg m-1">
+        Inscribete totalmente <span className="font-bold">GRATIS!</span>
+      </p>
+      <p className="bg-white/10 p-2 rounded-lg m-1">
+        Acumula puntos dictando tu rut en caja
+      </p>
+      <p className="bg-white/10 p-2 rounded-lg m-1">
+        Canjea tus puntos en caja por cualquier producto! 1 punto = 1 peso
+      </p>
+      <p className="bg-white/10 p-2 rounded-lg m-1">
+        Tambien puedes usar tus puntos para participar en concursos
+      </p>
+    </div>
+  </StrictMode>
+);
